e67127c285dfa3f08ce36067fb676f9842133b33
Yoga Darma <kadekyoga125@gmail.com>
2019-11-24 16:24:08 +0700

    route cors

--- routes/api.php
+++ routes/api.php
@@ -13,35 +13,33 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // Route::resource('users', 'UserController')->only(['store']);
+Route::group(['prefix' => 'pesenin', 'middleware' => 'cors'], function () {
+    Route::post('login', 'UserController@login');
+
+    Route::post('register', 'UserController@store');
+
+    Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
+        Route::group(['middleware' => ['user.loggedin']], function () {
+            Route::resource('user', 'UserController')->except(['store']);
+            Route::post('logout', 'UserController@logout');
+            Route::resource('menu', 'MenuController')->except(['store']);
+            Route::post('pesan', 'UserController@pesan');
 
 
-Route::post('login', 'UserController@login');
-
-Route::post('register', 'UserController@store');
-
-Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
-    Route::group(['middleware' => ['user.loggedin']], function () {
-        Route::resource('user', 'UserController')->except(['store']);
-        Route::post('logout', 'UserController@logout');
-        Route::resource('menu', 'MenuController')->except(['store']);
-        Route::post('pesan', 'UserController@pesan');
-
-
-        Route::group(['middleware' => ['admin.loggedin']], function () {
-            Route::resource('admin', 'AdminController')->only(['index']);
-            Route::resource('admin/store/menu', 'MenuController')->only(['store']);
-            Route::post('admin/update/menu/{menu}', 'MenuController@update')->name('update');
-            Route::resource('admin/store/meja', 'MejaController')->only(['store']);
-            Route::post('admin/update/meja', 'MejaController@updateMeja');
+            Route::group(['middleware' => ['admin.loggedin']], function () {
+                Route::resource('admin', 'AdminController')->only(['index']);
+                Route::resource('admin/store/menu', 'MenuController')->only(['store']);
+                Route::post('admin/update/menu/{menu}', 'MenuController@update')->name('update');
+                Route::resource('admin/store/meja', 'MejaController')->only(['store']);
+                Route::post('admin/update/meja', 'MejaController@updateMeja');
+            });
         });
     });
-});
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-
+    Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () { });
 });
